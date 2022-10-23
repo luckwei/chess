3204046ab15edfaed4b510cf08d440de35305aab
luckwei <luckwei123@gmail.com>
2022-10-24 05:05:01 +1100

    added few moves for pawn

--- chess/board.py
+++ chess/board.py
@@ -28,6 +28,7 @@ def empty_board() -> Grid:
 class Board:
     theme: tuple[str, str] = THEME.RED
     pieces: Grid = field(init=False, default_factory=empty_board)
+    to_move: PieceColor=field(init=False, default=PieceColor.WHITE)
 
     def __post_init__(self):
         self.update_from_fen()
@@ -67,18 +68,22 @@ class Board:
             None,
         )
 
-    def get_valid_moves(self, row: int, col: int) -> list[Position]:
-        return MOVE_LIST[self.piece(row, col).type](self, row, col)
-
     def __str__(self) -> str:
         pieces_str = [str(piece) for piece in self.pieces.values()]
         rows = ["".join(pieces_str[i * 8 : (i + 1) * 8]) for i in range(8)]
         return "\n".join(rows)
+    
+    def get_valid_moves(self, row:int, col:int) -> list[Position]:
+        # TODO: assert that chess piece color has to be this turn
+        piece = self.piece(row, col)
+        if piece.color != self.to_move:
+            return []
+        return MOVE_CALCULATOR[piece.type](self, row, col)
 
 
 ValidMoveCalculator = Callable[[Board, int, int], list[Position]]
 
-MOVE_LIST: dict[PieceType, ValidMoveCalculator] = {
+MOVE_CALCULATOR: dict[PieceType, ValidMoveCalculator] = {
     PieceType.ROOK: get_valid_moves_rook,
     PieceType.BISHOP: get_valid_moves_bishop,
     PieceType.KNIGHT: get_valid_moves_knight,
--- chess/move.py
+++ chess/move.py
@@ -1,12 +1,21 @@
 from __future__ import annotations
-
+from .piece import PieceColor
 import chess
 
 Position = tuple[int, int]
 
 
 def get_valid_moves_pawn(board: chess.Board, row: int, col: int) -> list[Position]:
-    ...
+    valid_moves = []
+    piece = board.piece(row, col)
+    direction = -1 if piece.color == PieceColor.WHITE else 1
+    
+    valid_moves.append((row+direction, col))
+    valid_moves.append((row+direction*2, col))
+    valid_moves.append((row+direction, col+1))
+    valid_moves.append((row+direction, col-1))
+    print(valid_moves)
+    return valid_moves
 
 
 def get_valid_moves_rook(board: chess.Board, row: int, col: int) -> list[Position]:
