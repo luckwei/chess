e4b686296c61eaecb25328d04405bb0822a329d4
luckwei <luckwei123@gmail.com>
2022-12-09 03:18:52 +1100

    Reorganized project to implement flask backend

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,12 +1,30 @@
 import Chessboard from "./components/Chessboard";
-import {Color} from "./gameLogic"
-const hi = Color.BLACK
+import { CastlingPermission, Color, Flag, Move } from "./gameLogic";
+
+class UM extends Map {
+  
+  set(key: [Color, Flag], value: any) {
+    return super.set(key.toString(), value);
+  }
+
+  get(key: [Color, Flag]){
+    return super.get(key.toString());
+  }
+  has(key: [Color, Flag]) {
+    return super.has(key.toString())
+  }
+  
+}
+
+let m = new Map()
+m.set(Color.WHITE, true)
+
 function App() {
   return (
     <main>
       <h1>Chessboard</h1>
-      <h1>{Color.other(hi)}</h1>
-      <Chessboard/>
+      <h1>{m.has(Color.WHITE)? "something":"there is nothing"}</h1>
+      <Chessboard />
     </main>
   );
 }
--- vite-project/src/gameLogic.ts
+++ vite-project/src/gameLogic.ts
@@ -1,20 +1,20 @@
-type Position = [number, number];
+export type Position = [number, number];
 
-function in_bounds(pos: Position): boolean {
+export function in_bounds(pos: Position): boolean {
   return Math.max(...pos) <= 7 && Math.min(...pos) >= 0;
 }
 
-function in_bounds_filter(lst: Position[]): Position[] {
+export function in_bounds_filter(lst: Position[]): Position[] {
   return lst.filter((e) => in_bounds(e));
 }
 
-enum Color {
+export enum Color {
   NONE = "none",
   WHITE = "white",
   BLACK = "black",
 }
 
-namespace Color {
+export namespace Color {
   export function dir(color: Color): number {
     return color == Color.WHITE ? -1 : color == Color.BLACK ? 1 : 0;
   }
@@ -32,28 +32,123 @@ namespace Color {
   }
 }
 
-abstract class Piece {
-  readonly isPiece: boolean
-  constructor(readonly color: Color) {
-    this.isPiece = color != Color.NONE
-  }
-  abstract moves(board: any, pos: any): any[];
+export abstract class Piece {
+  readonly isPiece: boolean;
 
+  constructor(readonly color: Color) {
+    this.isPiece = color != Color.NONE;
+  }
+  abstract moves(board: any, pos: Position): Move[];
 }
 
-class Empty extends Piece {
-  constructor(...args:any[]) {
-    super(Color.NONE)
+export class Empty extends Piece {
+  constructor() {
+    super(Color.NONE);
   }
   // Implement bool: bool implemented as isPiece property
-  moves(board: any, pos: any): any[] {
-    return []
+  moves(board: any, pos: Position): Move[] {
+    return [];
+  }
+}
+//TODO: CREATE OTHER PIECE TYPES
+export enum Flag {
+  NONE,
+  ENPASSANT_TRGT,
+  ENPASSANT,
+  CASTLE_QSIDE,
+  CASTLE_KSIDE,
+  LOSE_KING_PRIV,
+  LOSE_ROOK_PRIV,
+  PROMOTION,
+  // Flag loses bool property
+}
+export class Move extends Array<number> {
+  constructor(pos: Position, public flag: Flag = Flag.NONE) {
+    super();
+    this.push(...pos);
   }
 
+  add(other: Move | Position) {
+    return new Move([this[0] + other[0], this[1] + other[1]], this.flag);
+  }
+  // __add__/__radd__/delta implementation is now replaced by add method
 }
 
 
-// dir, other, back_rank missing
+export class CastlingPermission extends Map{
+  private static FENCASTLING = new Map<[Color, Flag], string>([
+    [[Color.WHITE, Flag.CASTLE_KSIDE], "K"],
+    [[Color.WHITE, Flag.CASTLE_QSIDE], "Q"],
+    [[Color.BLACK, Flag.CASTLE_KSIDE], "k"],
+    [[Color.BLACK, Flag.CASTLE_QSIDE], "q"],
+  ]);
 
-export { Color };
+  constructor(castlingPermStr: string = "KQkq") {
+    super();
+    CastlingPermission.FENCASTLING.forEach((castlingChar, colorFlag) => {
+      this.set(colorFlag, castlingPermStr.includes(castlingChar))
+    })
+  }
+
+  falsify(color: Color, flag: Flag = Flag.LOSE_KING_PRIV) {
+    if (flag == Flag.LOSE_KING_PRIV){
+      this.set([color, Flag.CASTLE_QSIDE], false)
+      this.set([color, Flag.CASTLE_QSIDE], false)
+      return
+    }
+    this.set([color, flag], false)
+  }
+  set(key: [Color, Flag], value: boolean) {
+    return super.set(key.toString(), value)
+  }
+  get(key: [Color, Flag]) {
+    return super.get(key.toString())
+  }
+}
+
+//TODO: POPULATE WITH PIECE TYPES
+const FEN_MAP = new Map<string, Piece>([[" ", new Empty()]]);
+
+export class Board extends Map{
+  colorMove: Color;
+  castlingPermission: CastlingPermission;
+  empassantTarget: Position;
+  allMovesCache: Map<Position, Piece>;
+
+  constructor(
+    fenString = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+  ) {
+    super();
+
+    //random
+
+    const splitStr = fenString.replace("/", "").split(" ");
+    let [boardConfig, colorMove, castlingPerm, enpassantTrgt, ...rest] =
+      splitStr;
+    this.colorMove = colorMove == "w" ? Color.WHITE : Color.BLACK;
+    this.castlingPermission = new CastlingPermission(castlingPerm);
+    this.empassantTarget = [
+      8 - parseInt(enpassantTrgt[1]),
+      "abcdefgh".indexOf(enpassantTrgt[0]),
+    ];
+    this.allMovesCache = new Map<Position, Piece>();
+    [..."12345678"].forEach(i => {
+      boardConfig = boardConfig.replace(i, " ".repeat(parseInt(i)))
+    })
+    //TODO: CHECK IF SETTING KEY AS ARRAY LEADS TO ERROR
+    for (const [index, pieceChar] of [...boardConfig].entries()) {
+      const pos: Position = [Math.floor(index / 8), index % 8];
+      this.allMovesCache.set(pos, FEN_MAP.get(pieceChar) ?? new Empty());
+    }
+
+    //TODO: IMPLEMENT OTHER METHODS
+    
+  }
+  set(key: Position, value: Piece) {
+    return super.set(key.toString(), value)
+  }
+  get(key: Position) {
+    return super.get(key.toString())
+  }
+}
 
